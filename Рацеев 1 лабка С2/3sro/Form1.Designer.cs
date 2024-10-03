4f55b9e50045cd2b0970077e40cade4841bf3145
Alimzhan <122010975+wersvet@users.noreply.github.com>
2024-10-03 19:15:10 +0500

    Add files via upload

---
+++ Рацеев 1 лабка С2/3sro/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace _3sro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.leave = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(9, 62);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(363, 571);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(421, 191);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(188, 77);
+            this.textBox1.TabIndex = 1;
+            // 
+            // output
+            // 
+            this.output.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.output.Enabled = false;
+            this.output.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.output.Location = new System.Drawing.Point(421, 301);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(188, 77);
+            this.output.TabIndex = 15;
+            this.output.Text = "Посчитать";
+            this.output.UseVisualStyleBackColor = false;
+            this.output.Click += new System.EventHandler(this.output_Click);
+            // 
+            // clear
+            // 
+            this.clear.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.clear.Enabled = false;
+            this.clear.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.clear.Location = new System.Drawing.Point(421, 406);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(188, 77);
+            this.clear.TabIndex = 16;
+            this.clear.Text = "Очистить";
+            this.clear.UseVisualStyleBackColor = false;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // leave
+            // 
+            this.leave.BackColor = System.Drawing.Color.Tomato;
+            this.leave.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.leave.Location = new System.Drawing.Point(421, 503);
+            this.leave.Name = "leave";
+            this.leave.Size = new System.Drawing.Size(188, 77);
+            this.leave.TabIndex = 17;
+            this.leave.Text = "Выйти";
+            this.leave.UseVisualStyleBackColor = false;
+            this.leave.Click += new System.EventHandler(this.leave_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::_3sro.Properties.Resources._3sro7;
+            this.pictureBox2.Location = new System.Drawing.Point(381, 62);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(228, 50);
+            this.pictureBox2.TabIndex = 19;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::_3sro.Properties.Resources._3sro5;
+            this.pictureBox1.Location = new System.Drawing.Point(9, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 51);
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(410, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 24);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Введите значение Z:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 653);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(500, 16);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Максимальное среди значений функции, имеющих целую часть, крат-ную Z";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 686);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(287, 16);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "количество таких максимальных значений";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(414, 586);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(188, 77);
+            this.textBox2.TabIndex = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(623, 711);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.leave);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button output;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button leave;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Рацеев 1 лабка С2/3sro/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3sro
+{
+    public partial class Form1 : Form
+    {
+        public struct User
+        {
+            public double znakX;
+            public double znakY;
+
+            public User(double _znakX, double _znakY)
+            {
+                znakX = _znakX;
+                znakY = _znakY;
+            }
+        }
+        List<User> users = new List<User>();  
+        
+        private const double A = -5.0;
+        private const double B = 5.0;
+        private const double H = 0.1;
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.KeyPress += textBox_KeyPress;
+            textBox1.TextChanged += textBox_TextChanged;
+        }
+        private void textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != ',' && e.KeyChar != '-')
+            {
+                e.Handled = true;
+            }
+            else if (e.KeyChar == '-' && ((TextBox)sender).SelectionStart > 0) // если это знак "-" и он не первый то блокируем
+            {
+                e.Handled = true;
+            }
+            else if (e.KeyChar == ',' && ((TextBox)sender).Text.Contains(",")) // если это запятая "," и ты уже ввел запятую, то больше вводить ее ты не модешь.
+            {
+                e.Handled = true;
+            }
+            else if (e.KeyChar == ',' && ((TextBox)sender).Text.Length == 0) // если ты ввел запятую и она первая, то вместо нее ставим "0,"
+            {
+                e.Handled = true;
+                ((TextBox)sender).Text = "0,";
+                ((TextBox)sender).SelectionStart = 2; // после изменение после какого символа пишешь дальше
+            }
+            else if (((TextBox)sender).Text == "-,")
+            {
+                e.Handled = true;
+                ((TextBox)sender).Text = "-0,";
+                ((TextBox)sender).SelectionStart = 3;
+            }
+            else if (((TextBox)sender).Text == "00")
+            {
+                e.Handled = true;
+                ((TextBox)sender).Text = "0,0";
+                ((TextBox)sender).SelectionStart = 3;
+            }
+            else if (((TextBox)sender).Text == "0")
+            {
+                e.Handled = true;
+                ((TextBox)sender).Text = "0,";
+                ((TextBox)sender).SelectionStart = 2;
+            }
+
+        }
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                output.Enabled = true;
+                clear.Enabled = true;
+            }
+            else
+            {
+                output.Enabled = false;
+                clear.Enabled = false;
+            }
+        }
+        private double ParseNumber(string input) // потому что мы не заменяем "-" в "0" сразу, а только когда он нажмет вычислить 
+        {
+            if (input == "-")
+            {
+                input = "0";
+            }
+            return double.Parse(input);
+        }
+        private void clear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+        private void leave_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private double Function(double x)
+        {
+            return x * Math.Exp(x) + 2 * Math.Sin(x) - Math.Sqrt(Math.Abs(Math.Pow(x, 3) - Math.Pow(x, 2)));
+        }
+
+        private void output_Click(object sender, EventArgs e)
+        {
+            double Z = double.Parse(textBox1.Text);
+            Z = Math.Truncate(Z);
+            DataTable table = new DataTable();
+            table.Columns.Add("Значение x", typeof(double));
+            table.Columns.Add("f(x)", typeof(double));
+
+            double maxResult = 0;
+            double dlyax = 0;
+            int count = 0;
+            int ii = 0;
+            //int craska = 1;
+            for (double x = A; x <= B; x += H)
+            {
+                double y = Function(x);
+                y = Math.Round(y, 4);
+                x = Math.Round(x, 4);
+                users.Add(new User(x, y));
+                table.Rows.Add(users[ii].znakX, users[ii].znakY);
+                double okrugY = Math.Truncate(y);
+                if (okrugY % Z == 0)
+                {
+                    if (y > maxResult)
+                    {
+                        maxResult = y;
+                        dlyax = Math.Round(x, 4);
+                        count = 1;
+                      //  craska = ii;
+                    }
+                    else if (y == maxResult)
+                    {
+                        count++;
+                    }
+                }
+                ii++;
+            }
+           // craska = (int)((dlyax * 10) + 50);
+            //dataGridView1.Rows[craska].DefaultCellStyle.BackColor = Color.Yellow;
+            if (maxResult == 0)
+            {
+                label2.Text = "среди значений функции, имеющих целую часть, Нет кратных Z";
+                label3.Text = "0";
+            }
+            table.Rows.Add(dlyax, Math.Floor(maxResult));
+            table.Rows.Add(dlyax, count);
+            dataGridView1.DataSource = table;
+            double faf = -70 + new Random().Next(101);
+            label2.Text = "Максимальное среди значений функции, имеющих целую часть, крат-ную Z: " + Math.Truncate(maxResult);
+            label3.Text = "количество таких максимальных значений: " + count;
+            textBox2.Text = faf.ToString();
+        }
+    }
+}
